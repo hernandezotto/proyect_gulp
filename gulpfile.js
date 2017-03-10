428ec26e7ab27df3e654f3469e5953c27498f77b
hernandezotto <hernandezottto@misena.edu.co>
2017-03-09 19:59:17 -0500

    pug y sass ya estan en proyect_gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const gulp = require('gulp'),
 pug = require ('gulp-pug');
-gulp.task('pug', function prepros(){    
-   return gulp.src('./preproceso/*.pug')
+gulp.task('pug', () =>
+ gulp.src('./preproceso/pug/*.pug')
     .pipe(pug({
         pretty: true
     }))
     .pipe(gulp.dest('./dist/'))
-});
+);
 gulp.task('default', () => {
-    gulp.watch('./preproceso/*.pug',['pug']);
+    gulp.watch('./preproceso/pug/*.pug',['pug']);
 });
+const gulp=reqire('gulp'),  
+    sass = require('gulp-sass');
+    gulp.task('sass',()=> 
+    gulp.src('./prepreceso/sass/*.sass')
+    .pipe(sass({
+        outputStyle:'expended'
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    );
+    gulp.task('default',()=>
+    gulp.watch('./prepreceso/sass/*.sass',['sass'])
+    );
